63ac8caaa4ffb8b6b8fa3738c634e8b7bf651aab
duhyundev <48434093+duhyundev@users.noreply.github.com>
2019-10-04 16:12:02 +0900

    Make directory dynamicly and take snapshot
     현재 날짜로 폴더를 생성하면서, 원하는 snapshot을 찍도록 수정했다.
    매번 모든 데이터를 scrap할수는 없고, 데이터를 획득한 시점과 해당 resoure
    별로 데이터를 보관해야한다. (물론, 임시적인 데이터이며, 결과적으로 json
    데이터로 만들고, airtable로 저장가능한 helper들이 필요하다.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+var mkdirp = require('mkdirp');
 var puppeteer = require('puppeteer');
 
-(async () => {
+const scrapper = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const github_id = 'duhyundev';
   const github_pw = process.env.PRIVATE_KEY;
 
+  const directory = new Date()
+    .toString()
+    .slice(0, 15)
+    .split(' ')
+    .join('-');
+
+  mkdirp(`${__dirname}/data/${directory}`, function(err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${directory} folder is created.`);
+    }
+  });
+
   await page.goto('https://github.com/login');
-  await page.screenshot({ path: 'data/step1.jpg' });
+  await page.screenshot({ path: `data/${directory}/step1.jpg` });
   await page.evaluate(
     (id, pw) => {
       document.querySelector('input[name="login"]').value = id;
@@ -20,15 +35,17 @@ var puppeteer = require('puppeteer');
   await page.click('input[name="commit"]');
 
   await page.waitFor(500);
-  await page.screenshot({ path: 'data/step2.jpg' });
+  await page.screenshot({ path: `data/${directory}/step2.jpg` });
 
   if (page.url() === 'https://github.com/session') {
     console.log('LOGIN FAIL');
   } else {
     await page.goto('https://github.com/codestates/help-desk/issues/001');
 
-    await page.screenshot({ path: 'data/step3.jpg' });
+    await page.screenshot({ path: `data/${directory}/step3.jpg` });
   }
 
   await browser.close();
-})();
+};
+
+scrapper();
